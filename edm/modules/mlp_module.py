Add confidence intervals, delong variance, and fix bug with the patience when producing waveforms

edm/modules/mlp_module.py
@@ -1,4 +1,3 @@
-
 from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score
 from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning.metrics.functional as metrics
@@ -29,7 +28,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 from edm.models.mlp_model import MLP
 from edm.dataloaders.mlp_dataloader import MLPDataLoader
-from edm.utils.measures import perf_measure, calculate_output_statistics
+from edm.utils.measures import perf_measure, calculate_output_statistics, calculate_confidence_intervals
 
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -305,8 +304,10 @@ def train_mlp(df_train, df_val, df_test, patience=10, dropout=True, inner_dim=64
     auprc_alt = auc(recall, precision)
     if verbose >= 1:
         auroc_train = calculate_output_statistics(final_y, final_preds)
+        calculate_confidence_intervals(final_y, final_preds)
     else:
         auroc_train = calculate_output_statistics(final_y, final_preds, show_plots=False)
+        calculate_confidence_intervals(final_y, final_preds)
     
     if save_predictions_path is not None:
         Path(f"{save_predictions_path}").mkdir(parents=True, exist_ok=True)
@@ -337,8 +338,10 @@ def train_mlp(df_train, df_val, df_test, patience=10, dropout=True, inner_dim=64
     auprc_alt = auc(recall, precision)
     if verbose >= 1:
         auroc_val = calculate_output_statistics(final_y, final_preds)
+        calculate_confidence_intervals(final_y, final_preds)
     else:
         auroc_val = calculate_output_statistics(final_y, final_preds, show_plots=False)
+        calculate_confidence_intervals(final_y, final_preds)
     val_aurocs.append(auroc_val)
     if verbose >= 1:
         print(f"VAL AUROC = {auroc_val} AUPRC = {auprc_alt} using data size {len(final_preds)} with {sum(final_y)} pos")
@@ -362,8 +365,10 @@ def train_mlp(df_train, df_val, df_test, patience=10, dropout=True, inner_dim=64
     final_preds, final_y = model.get_final_preds_and_y()
     if verbose >= 1:
         auroc_test = calculate_output_statistics(final_y, final_preds)
+        calculate_confidence_intervals(final_y, final_preds)
     else:
         auroc_test = calculate_output_statistics(final_y, final_preds, show_plots=False)
+        calculate_confidence_intervals(final_y, final_preds)
     precision, recall, _ = precision_recall_curve(final_y, final_preds)
     auprc_alt = auc(recall, precision)
     test_aurocs.append(auroc_test)
@@ -422,7 +427,8 @@ def test_mlp(df, model_path, dropout=True, inner_dim=64, embed_dim=322,
     
     for batch in tqdm(test_loader):
         x, y, patient_id = batch
-        output = model(torch.tensor([[x]]).to(device=device, dtype=torch.float))
+        x = x.to(device=device, dtype=torch.float)
+        output = model(x)
         preds = torch.sigmoid(output)
         preds = torch.squeeze(preds)
         preds = preds.cpu()
@@ -441,8 +447,10 @@ def test_mlp(df, model_path, dropout=True, inner_dim=64, embed_dim=322,
 
     if verbose >= 1:
         auroc_test = calculate_output_statistics(final_y, final_preds)
+        calculate_confidence_intervals(final_y, final_preds)
     else:
         auroc_test = calculate_output_statistics(final_y, final_preds, show_plots=False)
+        calculate_confidence_intervals(final_y, final_preds)
     precision, recall, _ = precision_recall_curve(final_y, final_preds)
     auprc_alt = auc(recall, precision)
     if verbose >= 1:

processing/prepare_ed_waveforms.py
@@ -66,7 +66,7 @@ def process_record(input_args):
             if waveform_type in info["supported_types"]:
                 window_size = int(waveform_length * fs)
                 attempt = 1
-                while num_waveforms_processed[waveform_type] < max_samples_per_patient and attempt < PATIENCE:
+                while num_waveforms_processed[waveform_type] < max_samples_per_patient:
 
                     start_offset = int(max(0, recommended_trim_start_sec * fs))
                     end_offset = int(min(len(waveform_base), recommended_trim_end_sec * fs))
@@ -76,7 +76,11 @@ def process_record(input_args):
                     pointer = np.random.randint(start_offset, max(1, end_offset - window_size))
 
                     waveform, quality = get_waveform(waveform_base, pointer, window_size, fs, should_normalize=should_normalize, bandpass_type=waveform_config["bandpass_type"], bandwidth=waveform_config["bandpass_freq"], target_fs=waveform_target_freq, ecg_quality_check=True)
-                    if quality == 0:
+                    
+                    if attempt >= PATIENCE:
+                        # We've run out of patience so we'll use whatever waveform we get
+                        print(f"[{i}/{total_rows}] {patient_id} waveform ran out of patience at {pointer}.")
+                    elif quality == 0:
                         print(f"[{i}/{total_rows}] {patient_id} waveform was empty at {pointer}. Trying again...")
                         attempt += 1
                         continue
@@ -94,6 +98,7 @@ def process_record(input_args):
 
                     pointer += window_size
                     num_waveforms_processed[waveform_type] += 1
+                assert len(num_waveforms_processed[waveform_type]) == max_samples_per_patient
 
         return waveforms
     except Exception as e: